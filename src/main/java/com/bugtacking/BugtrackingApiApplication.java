755412da875e4a5c320ae3be21b9cc69ffa911be
kripa <saiprasuna24011993@gmail.com>
2022-11-20 12:06:36 +0530

    jira-01-added comments-1 in master to test git push

--- src/main/java/com/bugtacking/BugtrackingApiApplication.java
+++ src/main/java/com/bugtacking/BugtrackingApiApplication.java
@@ -18,6 +18,8 @@ public class BugtrackingApiApplication {
 	public static void main(String[] args) {
 		//change made from git remote
 		System.out.println("changes made in IDE to test git push");
+
+        System.out.println("changes-1 made in IDE to test git push");
 		SpringApplication.run(BugtrackingApiApplication.class, args);
 		LOGGER.debug("Application has been started");
 	}
